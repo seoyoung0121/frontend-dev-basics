3656c8bdca7a9322ed040402ad57fa974d5db551
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-02-04 16:41:57 +0900

    javascript-practices - js 기초(객체)

--- javascript-practices/basics/ex08.02.js
+++ javascript-practices/basics/ex08.02.js
@@ -2,22 +2,63 @@
 Array 객체 함수: Array.prototype.*
 */
 
-var colors = ['black', 'white', 'yellow'];
-var fruits = ['apple', 'mango', 'banana'];
-
-
+var colors = ["black", "white", "yellow"];
+var fruits = ["apple", "mango", "banana"];
 
 console.log("============[02.01] concat");
+var a1 = fruits.concat(colors);
+console.log(a1);
+
 console.log("============[02.02] stack 지원");
+var color = colors.pop();
+console.log(color, colors);
+
+colors.push("red");
+console.log(colors);
+
 console.log("============[02.03] join");
+var s = fruits.join(":");
+console.log(s);
+
 console.log("============[02.04] reverse");
+console.log(fruits);
+fruits.reverse();
+console.log(fruits);
+
 console.log("============[02.05] shift");
+var numbers1 = [1000, 2000, 3000, 4000, 5000];
+var number = numbers1.shift();
+console.log(number, numbers1);
+
 console.log("============[02.06] slice");
+var numbers2 = numbers1.slice(1, 3);
+console.log(numbers2);
+
 console.log("============[02.07] splice(index, count)");
+console.log(fruits);
+var fruits2 = fruits.splice(0, 2);
+console.log(fruits2, fruits);
+
+// index에서 count 개를 삭제하고 replace를 대체한 후, 삭제된 요소를 배열을 반환
 console.log("============[02.07] splice(index, count, replace)");
+var a1 = [0, 1, 2, 3, 4];
+var s2 = a1.splice(1, 3, 10);
+console.log(s2, a1);
+
 console.log("============[02.07] splice: as a removeAt()");
+var a3 = [0, 1, 2, 3, 4];
+a3.splice(2, 1); // removeAt(2)
+console.log(a3);
+
 console.log("============[02.07] splice: as a insertAt()");
+var a4 = [0, 1, 2, 3, 5];
+a4.splice(4, 0, 4); //insertAt(4,4)
+console.log(a4);
+
 console.log("============[02.08] sort");
+
 console.log("============[02.09] unshift");
+
 console.log("============[02.10] indexOf");
+
 console.log("============[02.11] lastIndexOf");
--- javascript-practices/basics/ex08.03.js
+++ javascript-practices/basics/ex08.03.js
@@ -4,9 +4,43 @@
     ex) List 함수 지원하기
 
 */
+Array.prototype.remove = function (index) {
+  this.splice(index, 1);
+};
 
+Array.prototype.forEach = function (callback) {
+  for (var i = 0; i < this.length; i++) {
+    callback(this[i]);
+  }
+};
 
+Array.prototype.insert = function (index, value) {
+  if (value instanceof Array) {
+    // for (var i = 0; i < value.length; i++) {
+    //   this.splice(index + i, 0, value[i]);
+    // }
+    // 오류!!
+    // 콜백함수에서의 this는 어휘상의 this와 일치하지 않는다.
+    // value.forEach(function (e) {
+    //   this.splice(index++, 0, e);
+    // });
 
+    // 해결방법 1:
+    // var _this = this;
+    // value.forEach(function (e) {
+    //   _this.splice(index++, 0, e);
+    // });
+
+    //해결방법 2:
+    value.forEach(
+      function (e) {
+        this.splice(index++, 0, e);
+      }.bind(this)
+    );
+  } else {
+    this.splice(index, 0, value);
+  }
+};
 
 // List 함수 사용하기
 var a = [1, 2, 4];
@@ -18,5 +52,8 @@ console.log(a);
 a.remove(2);
 console.log(a);
 
-a.insert(2, ['a', 'b', 'c']);
+a.insert(2, ["a", "b", "c"]);
 console.log(a); // [1, 2, 'a', 'b', 'c', 4]
+
+//parseInt("10");
+console.log(global.parseInt(10));
